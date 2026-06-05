3c88743575a6562fae182c003d2d4d092d46613f
suvinson <suvinson1209@gmail.com>
2026-06-05 09:22:36 +0530

    code modified

--- Selection_sort.java
+++ Selection_sort.java
@@ -40,7 +40,24 @@ class Solution
             ar[min]=ar[i];
             ar[i]=temp;
         }
-
         return ar;
     }
 }
+/*
+for(int i=0;i<n;i++)
+{
+    int min = arr[i];
+    int min_index = i;
+    for(int j=i=1;j<n;j++)
+    {
+        if(arr[j]<arr[index])
+        {
+            min = arr[j];
+            min_index  = j;
+        }
+    }
+    int temp = arr[j];
+    arr[j] = arr[min_index];
+    arr[min_index] = temp;
+}
+*/
